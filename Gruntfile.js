407e00d95f8282e54ead2601fd268218596a8b54
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2017-01-23 12:53:41 -0500

    corrections on builder

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,6 @@ module.exports=(grunt)=>{
               , 'less:server'
               , 'uglify:server'
               , 'copy:server'
-              , 'cssmin:server'
             ]
         }
       , pug:{
@@ -85,7 +84,7 @@ module.exports=(grunt)=>{
                   , paths:['bower_components']
                 }
               , files:{
-                    'dist/client/t1.css'
+                    'dist/css/t1.css'
                   : 'client/less/t1.less'
                 }
             }
@@ -93,16 +92,8 @@ module.exports=(grunt)=>{
       , uglify:{
             server:{
                 files:[{
-                    'dist/notifier/client/js/jquery.min.js':[
-                        'bower_components/jquery/dist/jquery.min.js'
-                    ]
-                },{
-                    'dist/notifier/client/js/socket.io.min.js':[
-                        'bower_components/socket.io-client/socket.io.js'
-                    ]
-                },{
-                    'dist/notifier/client/js/notifier.min.js':[
-                        'client/js/worker.js'
+                    'dist/js/t1.min.js':[
+                        'client/js/t1.js'
                     ]
                 }]
             }
@@ -111,34 +102,13 @@ module.exports=(grunt)=>{
             server:{
                 files:[{
                     src:'client/favicon.ico'
-                  , dest:'dist/notifier/client/favicon.ico'
+                  , dest:'dist/favicon.ico'
                 },{
                     expand:true
-                  , src:['core/*.js']
-                  , dest:'dist/notifier/'
-                },{
-                    src:'server/notifier.io.js'
-                  , dest:'dist/notifier/server/notifier.io.js'
-                },{
-                    src:'config/notifier.js'
-                  , dest:'dist/notifier/config/notifier.js'
-                },{
-                    src:'package/notifier.json'
-                  , dest:'dist/notifier/package.json'
+                  , cwd:'client/png/'
+                  , src:['**']
+                  , dest:'dist/png/'
                 }]
-            }
-        }
-      , cssmin:{
-            server:{
-                files:{
-                    'dist/master/client/style.css':[
-                        'dist/master/client/style.css'
-                      , 'bower_components/angular-resizable/'
-                            +'angular-resizable.min.css'
-                      , 'bower_components/foundation-datepicker/css/'
-                            +'foundation-datepicker.min.css'
-                    ]
-                }
             }
         }
     });
@@ -147,7 +117,6 @@ module.exports=(grunt)=>{
     grunt.registerTask('build',[
         'clean:server'
       , 'concurrent:server'
-      , 'cssmin:server'
     ]);
     grunt.registerTask('serve',[
         'develop:server'
